e1325b5e09e6fab25ba5ed20b18269f693202d8a
Christian Contreras <82068788+degoleiv@users.noreply.github.com>
2024-06-10 23:42:39 -0500

    reinicio

--- Backend/README.md
+++
@@ -1,2 +0,0 @@
-# BackendPracticas
-codigo del backend para el proyecto de practicas profesionales
--- Backend/practicas/app/init.py
+++
@@ -1,4 +0,0 @@
-from dotenv import load_dotenv
-from genesis.query import search_student
-from genesis.flatten import flatten_dict
-load_dotenv()
